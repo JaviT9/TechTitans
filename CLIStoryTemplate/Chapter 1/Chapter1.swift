e1c648b502d44f78d79a89d8ab47cb0e81f80068
Justin <justin2525@me.com>
2023-10-18 11:14:29 -0400

    removed Javi's story

--- CLIStoryTemplate/Chapter 1/Chapter1.swift
+++ CLIStoryTemplate/Chapter 1/Chapter1.swift
@@ -5,9 +5,4 @@
 
 import Foundation
 
-func chapterOne() {
-    print("Test for the TechTitans")
-    print("The protagonist male or female? Duo?")
-    print("I dont even know anymore im tired")
-    // Your portion of the story goes here
-}
+func chapterOne() { }
