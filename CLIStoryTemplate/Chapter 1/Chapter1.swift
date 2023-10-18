254b3b14140b61678558049261d7992acafa3eef
Javier Torres <jtorres23@msu.idserve.net>
2023-10-18 10:51:46 -0400

    Tested Print function

--- CLIStoryTemplate/Chapter 1/Chapter1.swift
+++ CLIStoryTemplate/Chapter 1/Chapter1.swift
@@ -6,5 +6,8 @@
 import Foundation
 
 func chapterOne() {
+    print("Test for the TechTitans")
+    print("The protagonist male or female? Duo?")
+    print("I dont even know anymore im tired")
     // Your portion of the story goes here
 }
