3aaa8621a80a741b897db10392ad1d2861ca832e
jala <jalaundalewis@gamail.com>
2023-10-20 11:15:38 -0400

    story

--- CLIStoryTemplate/Chapter 3/Chapter3.swift
+++ CLIStoryTemplate/Chapter 3/Chapter3.swift
@@ -6,5 +6,9 @@
 import Foundation
 
 func chapterThree() {
-    // Your portion of the story goes here
+    let name = "your name"
+    print ("hey my name is")
+    The golden chip Was stolen from the mother of all computers. Because of that ,an electromagnetic storm is on the rise to destroy planet Zenith.The Golden chip being restored is the only way to stop the storm. Titans long journey to retrieve the golden chip, chances are slim. He knew only one person that could calm the storm so he can get the golden chip. Techtor, Titan knew of the things Techtor invented. He lived in a cave that contained the ruins of an ancient city . A city that had all power to manipulate the electric currents in the air. Titan knew of all obstacles he had to achieve in order to save planet Zenith. So his first stop was to the eldest techllow tree , deep within the the computer forest. When he approached the techllow tree it was only within matter of seconds the tree up held Titan with its brilliant rays of light and knowledge. Once released from the tree it was an abundance of knowledge and he was ready for his adventure. But before he left, the Techllow tree said to him, your weekness is power , too much of it and it will kill you, it’ll all be for nothing. Titan left and found his old pal the inventor, Techtor. Techtor and titan began their mission.  As they got closer to the electric sea they noticed something. They saw red rising with blue lightning. Its the tech dragon . Titan went heads up with the tech dragon .There was lightning and fire everywhere. Titan was almost down . With Techtor by Titans side  the dragon had no chance. Techtor waved his arms powerfully and lifted is body ,eyes closed. A  strong circle of wind bursted .It made the dragon week. Titan was able to retrieve the golden chip off the dragons back. the dragon then fell into the electric ocean and died, knowing it wasnt the water that killed the dragon. Titan then remembered the Tehllow's words . Weakness is power to much of it and it will kill you.
+
+    //
 }
