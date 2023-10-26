0b16ca349d24aa0b4212732278324cc476b708bf
Javier Torres <jtorres23@msu.idserve.net>
2023-10-26 11:40:43 -0400

    Restored Chapter 4

--- CLIStoryTemplate/Chapter 4/Chapter4.swift
+++ CLIStoryTemplate/Chapter 4/Chapter4.swift
@@ -5,6 +5,43 @@
 
 import Foundation
 
+
+let heroName: String = "Pixel"
+
 func chapterFour() {
     // Your portion of the story goes here
+    // does not have to be long asf simple 3-4 sentences to add to others story
+    // must use at least 2 new, learned functions
+    // Make sure story is cohesive to what your other team mates are making
+    // Possible death scene
+    print("The electric sea")
+    print("Finally, the last step of my journey, once i aquire the Heart of the Sea i can finally go home and save Zenith")
+    print("Our hero reaches the storm shore, the entrance to the electric sea.")
+    print("Our hero finds a boat and begins their treck to the the abyss trench, the center most ferocious area within the sea")
+    print("Within the trench, there is a glowing orb in its center, a raging, endless thunderstorm")
+    print("Under the glowing orb lay the text: BE WARNED ONCE THE HEART IS REMOVED, THE GURADIAN WILL AWAKEN.")
+    print("The final challenge/Climax")
+    print("Character completes goal and heads back home")
+    myStory()
+    myStory2()
+    myStory3()
+    // How to incorporate conditionals (if/ else statements) in the story, still need variables (Var- which is something that can be changed) also need bool (true or false statement
+    // So far i have incorporated functions and constants (let)
+}
+
+func myStory() {
+    print("Test \(heroName)")
+    print("Idek")
+    print("I think i got it")
+}
+func myStory2() {
+    print("Oh")
+    print("My")
+    print("Goodness")
+}
+func myStory3() {
+    print("By")
+    print("Golly")
+    print("I got it, i trully got it")
+    
 }
